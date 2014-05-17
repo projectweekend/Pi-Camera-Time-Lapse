92358eb52cca16c8b13fb0cfcfea256b466875b3
Brian Hines <brian@projectweekend.net>
2014-05-17 16:49:34 -0500

    It's not a function...duh

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ with open('settings.yml') as settings_file:
 
 if __name__ == '__main__':
     with picamera.PiCamera() as camera:
-        camera.resolution(IMAGE.resolution_x, IMAGE.resolution_y)
+        camera.resolution = (IMAGE.resolution_x, IMAGE.resolution_y)
         time.sleep(2)
         output_file = IMAGE.directory + '/img{counter:03d}.jpg'
         capture = camera.capture_continuous(output_file, quality=IMAGE.quality)
