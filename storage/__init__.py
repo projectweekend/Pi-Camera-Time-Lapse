282d054890cef539a69b008eaff882e16c2e0a47
Brian Hines <brian@projectweekend.net>
2014-05-18 09:24:33 -0500

    Function to check token before starting time lapse

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,4 +1,18 @@
+import dropbox
+
+
 DROPBOX_KEY_FILE = "./dropbox.txt"
+
+
+def has_valid_dropbox_token():
+    try:
+        with open(DROPBOX_KEY_FILE, 'r') as f:
+            dropbox_token = f.read()
+            client = dropbox.client.DropboxClient(dropbox_token)
+            client.account_info()
+    except (IOError, dropbox.client.ErrorResponse):
+        return False
+    return True
 
 
 def load_dropbox_key():
@@ -14,5 +28,3 @@ def save_dropbox_key(dropbox_key):
     with open(DROPBOX_KEY_FILE, 'w+') as f:
         f.write(dropbox_key)
     return dropbox_key
-
-
