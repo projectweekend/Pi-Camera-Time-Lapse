a8bd9defcf3359296acf7633041b036213868075
Brian Hines <brian@projectweekend.net>
2014-05-19 18:46:10 -0500

    Make getting started easier with a handy script

---
+++ install.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import subprocess
+
+
+def sudo(command_text):
+    parts = ['sudo']
+    parts.extend(command_text.split(command_text))
+    subprocess.call(parts)
+
+
+def apt_get_install(package_name):
+    command_text = "apt-get -y install {0}".format(package_name)
+    sudo(command_text)
+
+
+def main():
+    # Install system dependencies
+    sudo("apt-get update")
+    sudo("apt-get -y upgrade")
+    apt_get_install("upstart")
+    apt_get_install("python-dev")
+    apt_get_install("python-pip")
+
+    # Setup the virtualenv
+    subprocess.call(["pip", "install", "virtualenv"])
+    subprocess.call(["virtualenv", "env", "--no-site-packages"])
+    subprocess.call(["source", "./env/bin/activate"])
+    subprocess.call(["pip", "install", "-r", "requirements.txt"])
+
+    # Make default images folder
+    subprocess.call(["mkdir", "/home/pi/images"])
+
+    # Copy Upstart scripts
+    subprocess.call(["cp", "upstart/dropbox-worker.conf", "/etc/init"])
+    subprocess.call(["cp", "upstart/time-lapse.conf", "/etc/init"])
+
+    print("Installation complete!")
+    print("Please reboot your Raspberry Pi :)")
+
+
+if __name__ == '__main__':
+    main()
