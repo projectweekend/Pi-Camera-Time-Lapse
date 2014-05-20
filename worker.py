991d9c996a7279c0982df12f5494f3575e05de0a
Brian Hines <brian@projectweekend.net>
2014-05-20 06:07:06 -0500

    Check for valid Dropbox token when worker starts

--- worker.py
+++ worker.py
@@ -1,9 +1,10 @@
 import glob
 from settings import IMAGE
-from uploader import DropboxWorker
+from uploader import DropboxWorker, has_valid_dropbox_token
 
 
 if __name__ == '__main__':
-    while True:
-        files_to_upload = glob.glob(IMAGE.directory + "/*.jpg")
-        DropboxWorker(files_to_upload).work()
+    if has_valid_dropbox_token():
+        while True:
+            files_to_upload = glob.glob(IMAGE.directory + "/*.jpg")
+            DropboxWorker(files_to_upload).work()
