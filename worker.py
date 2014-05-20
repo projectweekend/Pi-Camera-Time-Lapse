e9eef74741965c3e3cad1fe40297b682024f501f
Brian Hines <brian@projectweekend.net>
2014-05-20 06:08:52 -0500

    Re organize worker

--- worker.py
+++ worker.py
@@ -3,8 +3,12 @@ from settings import IMAGE
 from uploader import DropboxWorker, has_valid_dropbox_token
 
 
-if __name__ == '__main__':
+def main():
     if has_valid_dropbox_token():
         while True:
             files_to_upload = glob.glob(IMAGE.directory + "/*.jpg")
             DropboxWorker(files_to_upload).work()
+
+
+if __name__ == '__main__':
+    main()
