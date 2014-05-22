82b75c268534ef79e5dfe5f37b9086968ae01b96
Brian Hines <brian@projectweekend.net>
2014-05-22 06:44:22 -0500

    Remove old settings stuff

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,12 +1,6 @@
 import glob
 import yaml
-import utils
 
-
-with open('settings.yml') as settings_file:
-    YML = yaml.safe_load(settings_file)
-    IMAGE = utils.Settings(YML['image'])
-    SNAP = utils.Settings(YML['snap'])
 
 DROPBOX_TOKEN_FILE = "./dropbox.txt"
 WORKING_DIRECTORY = "/home/pi/time-lapse"
