f5a8192f2fc70174c1ed011a447f538f360b4a1c
Brian Hines <brian@projectweekend.net>
2014-05-24 17:26:45 -0400

    Update time_lapse file

--- time_lapse.py
+++ time_lapse.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
 
 import time
-import picamera
+from camera import ConfigurableCamera
 from threading import Thread
 from uploader import upload_file
 from settings import Job, IMAGES_DIRECTORY
 
 
 def main():
+
     job = Job()
     if job.exists():
-        resolution_x = job.image_settings.resolution_x
-        resolution_y = job.image_settings.resolution_y
-        quality = job.image_settings.quality
+
         interval = job.snap_settings.interval
         total = job.snap_settings.total
+        quality = job.image_settings.quality
         file_prefix = job.image_settings.prefix
         output_file = IMAGES_DIRECTORY + '/' + file_prefix + '_{counter:03d}.jpg'
-        with picamera.PiCamera() as camera:
-            camera.resolution = (resolution_x, resolution_y)
-            time.sleep(2)
+
+        with ConfigurableCamera(job=job) as camera:
             capture = camera.capture_continuous(output_file, quality=quality)
+
             for i, file_name in enumerate(capture):
                 Thread(target=upload_file, args=(file_name,)).start()
+
                 if i == total - 1:
                     job.archive()
                     break
+
                 time.sleep(interval)
 
 
