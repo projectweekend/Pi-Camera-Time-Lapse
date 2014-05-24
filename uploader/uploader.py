4a6a727645f0a85a7c5a7d619e05f30f8d3ebd6b
Brian Hines <brian@projectweekend.net>
2014-05-23 20:46:40 -0500

    Reorganize uploader

---
+++ uploader/uploader.py
@@ -0,0 +1,41 @@
+import os
+import glob
+import logging
+import dropbox
+from dropbox.client import DropboxClient, ErrorResponse
+import settings
+from settings import DROPBOX_TOKEN_FILE
+
+
+def load_dropbox_token():
+    with open(DROPBOX_TOKEN_FILE, 'r') as f:
+        dropbox_token = f.read()
+    return dropbox_token
+
+
+def has_valid_dropbox_token():
+    try:
+        with open(DROPBOX_TOKEN_FILE, 'r') as f:
+            dropbox_token = f.read()
+            client = dropbox.client.DropboxClient(dropbox_token)
+            client.account_info()
+    except (IOError, ErrorResponse):
+        return False
+    return True
+
+
+def get_files_to_upload():
+    return glob.glob(settings.IMAGES_DIRECTORY + "/*.jpg")
+
+
+def upload_file(path):
+    access_token = load_dropbox_token()
+    client = DropboxClient(access_token)
+    name = path.split("/")[-1]
+    with open(path, 'rb') as data:
+        try:
+            client.put_file(name, data)
+        except Exception as e:
+            logging.exception(e)
+        else:
+            os.remove(path)
