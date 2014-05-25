7afa775c3d1b8f27b375598150d39b13e105420f
Brian Hines <brian@projectweekend.net>
2014-05-24 20:44:47 -0400

    Rework time lapse

--- time_lapse.py
+++ time_lapse.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-import time
 from camera import ConfigurableCamera
-from threading import Thread
-from uploader import upload_file
 from settings import Job, IMAGES_DIRECTORY
 
 
@@ -11,24 +8,10 @@ def main():
 
     job = Job()
     if job.exists():
-
-        interval = job.snap_settings.interval
-        total = job.snap_settings.total
-        quality = job.image_settings.quality
         file_prefix = job.image_settings.prefix
         output_file = IMAGES_DIRECTORY + '/' + file_prefix + '_{counter:03d}.jpg'
-
         with ConfigurableCamera(job=job) as camera:
-            capture = camera.capture_continuous(output_file, quality=quality)
-
-            for i, file_name in enumerate(capture):
-                Thread(target=upload_file, args=(file_name,)).start()
-
-                if i == total - 1:
-                    job.archive()
-                    break
-
-                time.sleep(interval)
+            camera.time_lapse(output_file)
 
 
 if __name__ == '__main__':
