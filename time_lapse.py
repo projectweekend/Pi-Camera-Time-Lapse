bcbbca11a00113e30b691ab79c13e78fb0993d66
Brian Hines <brian@projectweekend.net>
2014-05-31 10:31:31 -0500

    Fix settings references

--- time_lapse.py
+++ time_lapse.py
@@ -8,7 +8,7 @@ def main():
 
     job = Job()
     if job.exists():
-        file_prefix = job.image_settings.prefix
+        file_prefix = job.settings.file_prefix
         output_file = IMAGES_DIRECTORY + '/' + file_prefix + '_{counter:03d}.jpg'
         with ConfigurableCamera(job=job) as camera:
             camera.time_lapse(output_file)
